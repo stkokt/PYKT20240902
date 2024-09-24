601863dd78790c7294d0ea84c5c11606017fb120
stkokt <stefan.koschnik@mein-karrieretutor.de>
2024-09-24 12:33:41 +0200

    Update Uebung Listen 3.py

--- Uebung Listen 3.py
+++ Uebung Listen 3.py
@@ -12,14 +12,13 @@ liste1_2=[76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 96, 98, 1.1, 100, 102, 104, 10
 ind1_1=0
 ind1_2=0
 for x in liste1_1:
-    if str(x).isdecimal():
+    if not str(x).isdecimal():  # if type(x) == float   würde auch funktionieren
         ind1_1=liste1_1.index(x)
 for x in liste1_2:
-    if str(x).isdecimal():
+    if not str(x).isdecimal():
         ind1_2=liste1_2.index(x)
-liste1_3=liste1_1[:ind1_1]+liste1_2[ind1_2:]
-print(liste1_2[ind1_2], ind1_2, len(liste1_2))
-
+liste1_3=liste1_1[:ind1_1]+liste1_2[ind1_2+1:]  # liste1_2[ind1_2+1:]   wir geben Liste 1_2 einen um 1 versetzten Offset gegenüber dem gefundenen Index
+print(liste1_3)
 
 
 # 2 Erzeuge eine Liste bis 100, in der statt einer durch 5 teilbaren Zahl eine Liste eingefügt wird, 
@@ -27,17 +26,17 @@ print(liste1_2[ind1_2], ind1_2, len(liste1_2))
 
 print("Aufgabe 2\n")
 
-liste2=[x for x in range(100)]
+liste2=[x for x in range(101)]      # Liste per LC erstellt
 
-tmpList=[]
-listeOfFive=[]
+tmpList=[]      # Hilfsliste
+listeOfFive=[]  # Zielliste
 
 for x in liste2:    
     if x%5==0:
-        tmpList.append(liste2[x])
-        listeOfFive.append(tmpList.copy())  # Hier Kopie statt Original, weil Listen mutable!
+        tmpList.append(liste2[x])   # Hilfsliste wird um 5-teilbare Zahl erweitert
+        listeOfFive.append(tmpList.copy())  # Hilfsliste wird in der Zielliste eingefügt. Hier Kopie statt Original, weil Listen mutable!
     else:
-        listeOfFive.append(liste2[x])
+        listeOfFive.append(liste2[x])   # wenn nicht 5-teilbar, wird einfach nur die Zahl der Zielliste hinzugefügt.
 
 print(listeOfFive)
 
@@ -45,16 +44,16 @@ print(listeOfFive)
 
 """
 def listOf5(x):
-    tmpList=[]
-    if x%5==0:
-        for x in range(x+1):
-            if x%5==0:
-                tmpList.append(x)
-        return tmpList
+    tmpList=[]      # Hilfsliste
+    if x%5==0:      # wenn x teilbar 5:
+        for x in range(x+1):    # gehe durch die gesamte Zahlenreihe bis inklusive der aktuellen Zahl
+            if x%5==0:          # und sollte die jeweile Zahl teilbar 5 sein
+                tmpList.append(x)   # und hänge sie in die Hilfsliste
+        return tmpList              # gebe die Hilfsliste zurück
     else:
-        return x
+        return x                    # oder gebe die Zahl zurück, wenn die Bedingungen nicht zutrafen
     
-listeOfFive=[listOf5(x) for x in range(100)]
+listeOfFive=[listOf5(x) for x in range(100)]    # Funktion wird in einer List Comprehension angewandt
 """
 
 # 3 Erzeuge ein Liste aus zufälligen 3-stelligen Zahlen (random Modul). Entferne von jeder die erste und die dritte Ziffer und quadriere die mittlere.
@@ -62,9 +61,14 @@ listeOfFive=[listOf5(x) for x in range(100)]
 
 print("Aufgabe 3\n")
 import random
-liste3=[random.randint(100, 999) for x in range(10)]
+liste3=[random.randint(100, 999) for x in range(10)]    # Erforderliche Liste wird erstellt.
 print(liste3, "\n")
-liste3squared=[(int(list(str(x))[1]))**2 for x in liste3]
+liste3squared=[(int((str(x))[1]))**2 for x in liste3]
+
+# Zeile 66: (str(x))[1]         str(x) wandelt die Zahl in einen String um. Auch Strings haben Indizes.
+#                               Bei einem 3-stelligen String ist (str(x))[1] die Mitte.
+#           int((str(x))[1])    int() wandelt das Ergebnis in einen Integerwert um, der dann quadriert
+#                               und in die Zielliste übernommen wird. 
 
 # liste6squared=[((x-(x//100*100))//10)**2 for x in liste6]     # Alternative Gordon
 
@@ -84,9 +88,9 @@ print("\ngeaenderte Liste:\n", liste3squared, "\n")
 
 import random
 
-
-lotto=[x for x in range(1, 50)]
-tip = [5,18,29,45,37,7]
+lotto=[x for x in range(1, 50)]     # Liste 1 - 49 wird erstellt
+tip = [5,18,29,45,37,7]             # Das ist unser Tip (hart codiert, natürlich könnte auch er random sein)
+# Variablen für die jeweiligen Treffer
 t3=0
 t4=0
 t5=0
@@ -107,6 +111,7 @@ for n in range(1000000):
         case 6:
             t6+=1
 print(f"Dreier: {t3}, Vierer: {t4}, Fünfer: {t5}, Sechser: {t6}")
+print(t3+t4+t5+t6)
 
 #4 Erzeuge die Liste [[[1,2,3],[4,5,6],[7,8,9]], [10,11,12],[13,14,15],[16,17,18]], [18,20,21],[22,23,24],[25,26,27]]]
 #   mittels LC
